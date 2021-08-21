b404bdaf6354475b7f2ba4a8b5f4a65e560476b7
Anne Nicolle Zimmermann <anicolle@everis.com>
2021-08-20 23:35:20 -0300

    initial commit

---
+++ src/main/java/Base.java
@@ -0,0 +1,23 @@
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Base {
+    public static AndroidDriver<AndroidElement> capabilities() throws MalformedURLException {
+        File apk = new File("C:\\Users\\anicolle\\eclipse-workspace\\Tutorial\\src\\main\\java\\ApiDemos-debug.apk");
+        DesiredCapabilities cap = new DesiredCapabilities();
+
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "device");
+        cap.setCapability(MobileCapabilityType.APP, apk.getAbsolutePath());
+        cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"),cap);
+
+        return driver;
+    }
+}
