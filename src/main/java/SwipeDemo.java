066fa7c1288ba23c7d17a9808e6e58b0e63bb12e
Anne Nicolle Zimmermann <anicolle@everis.com>
2021-08-21 23:54:09 -0300

    using swipe in a clock example

---
+++ src/main/java/SwipeDemo.java
@@ -0,0 +1,24 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class SwipeDemo extends Base {
+
+    public static void main(String[] args) throws MalformedURLException {
+        AndroidDriver<AndroidElement> driver = capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        driver.findElementByXPath("//android.widget.TextView[@text='Date Widgets']").click();
+        driver.findElementByAndroidUIAutomator("text(\"2. Inline\")").click();
+        //driver.findElementByXPath("//android.widget.TextView[@text='2. Inline']");
+        driver.findElementByXPath("//*[@content-desc='10']").click();
+
+
+
+    }
+
+}
