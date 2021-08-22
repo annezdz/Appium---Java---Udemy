e23eed090591d342ebacae18ddbadd14b62fc457
Anne Nicolle Zimmermann <anicolle@everis.com>
2021-08-21 22:30:03 -0300

    Tap gesture using Touch Anction Class (work)

---
+++ src/main/java/Gestures.java
@@ -0,0 +1,36 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.TapOptions;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.ElementOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+import static io.appium.java_client.touch.TapOptions.tapOptions;
+import static io.appium.java_client.touch.offset.ElementOption.element;
+
+public class Gestures extends Base{
+    public static void main(String[] args) throws MalformedURLException {
+
+        AndroidDriver<AndroidElement> driver = capabilities();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElementByXPath("//android.widget.TextView[@text='Views']").click();
+        By expandList = MobileBy.AccessibilityId("Expandable Lists");
+
+        TouchAction action = new TouchAction(driver);
+        action.tap(TapOptions.tapOptions().withElement(ElementOption.element(driver.findElement(expandList)))).perform();
+
+
+//        TouchAction t = new TouchAction(driver);
+//        AndroidElement expandLinst = driver.findElementByAccessibilityId("Expandable Lists");
+//        t.tap(tapOptions().withElement(element(expandLinst))).perform();
+
+    }
+}
